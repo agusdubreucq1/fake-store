c9281d3e6c3d92f3c98471d4e2e8945f3ecbc051
agusdubreucq1 <adubreucq@frba.utn.edu.ar>
2023-12-27 22:48:49 -0300

    localstorage, readme y estilos

--- src/store/carrito.ts
+++ src/store/carrito.ts
@@ -7,12 +7,14 @@ interface CarritoState {
     removeFromCarrito: (id: number) => void;
 }
 
-export const useCarrito = create<CarritoState>((set) => ({
-    carrito: [],
+export const useCarrito = create<CarritoState>((set, get) => ({
+    carrito: localStorage.getItem('carrito') ? JSON.parse(localStorage.getItem('carrito')!) : [],
     addToCarrito: (product: Product) =>{
         set((state) => ({carrito: [...state.carrito, product]}))
+        localStorage.setItem('carrito', JSON.stringify(get().carrito))
     },
     removeFromCarrito(id: number) {
         set((state) => ({carrito: state.carrito.filter((product) => product.id !== id)}))
+        localStorage.setItem('carrito', JSON.stringify(get().carrito))
     },
 }))
